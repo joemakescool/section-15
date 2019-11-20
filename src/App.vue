0968a67a87a5a892691a0d3c95e0fbc90da15375
joebarker <joebarker@utah.gov>
2019-11-20 16:33:03 -0700

    Starting section-15 almost done

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,25 @@
 <template>
   <div id="app">
 
+    <div class="form-group">
+      <label>
+        <input type="text" class="form-control" v-model="user.username">
+      </label>
+    </div>
+
+    <div class="form-group">
+      <label>
+        <input type="text" class="form-control" v-model="user.email">
+      </label>
+    </div>
+
+    <button class="btn btn-primary" v-on:click="submit">SUBMIT</button>
+    <hr>
+    <button class="btn-dark" @click="fetchData">GET DATA</button>
+    <ul class="list-group">
+      <li class="list-group-item" v-for="user in users" :key="user.id">{{ user.username }} | {{ user.email }}</li>
+    </ul>
+
   </div>
 </template>
 
@@ -8,9 +27,68 @@
 
 export default {
   name: 'app',
+
   components: {
 
-  }
+  },
+
+  data: () => ({
+    user: {
+      username: '',
+      email: '',
+    },
+
+    users: [],
+    resource: {}
+
+  }),
+
+  methods: {
+    created() {
+      const customActions = {
+        saveAlt: {method: 'POST', url: 'alternative.json'}
+      };
+
+      this.resource = this.$resource('data.json', {}, customActions);
+    },
+
+    submit() {
+      // this.$http.post('https://vuejs-https-bdc63.firebaseio.com/data.json', this.user)
+      //         .then((response)=> {
+      //           // eslint-disable-next-line no-console
+      //           console.log(response);
+      //         }, (error)=> {
+      //           // eslint-disable-next-line no-console
+      //           console.error(error);
+      //         }); // send data to firebase. .json required
+      // // eslint-disable-next-line no-console
+      // console.log('Submitted!' + this.user)
+      //this.resource.save({}, this.user);
+      this.resource.saveAlt(this.user);
+
+    },
+
+    fetchData() {
+      this.$http.get('data.json')
+              .then( (response)=> {
+                return response.json(); // from vue-resource, gives back a promise
+              }).
+              then((data)=> {
+        // eslint-disable-next-line no-console
+                console.log(data); // Promise object extracted
+                const resultsArray = [];
+                for (let key in data) {
+                  resultsArray.push(data[key]);
+                }
+                this.users = resultsArray;
+        // eslint-disable-next-line no-console
+                console.log(this.users);
+      });
+    },
+
+  },
+
+
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,35 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+// Storage
+import VueResource from 'vue-resource'
+Vue.use(VueResource);
+
+// Database Connection
+Vue.http.options.root = 'https://vuejs-https-bdc63.firebaseio.com/'; // makes a global url
+Vue.http.interceptors.push((request, next)=> {
+  // eslint-disable-next-line no-console
+  console.log(request);
+  if (request.method === 'POST'){
+    request.method = 'PUT';
+  }
+  next(response => {
+    response.json = () => {
+      return {
+        messages: response.body
+      }
+    }
+  });
+});
+
+// Style
+import BootstrapVue from "bootstrap-vue";
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue);
+
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
